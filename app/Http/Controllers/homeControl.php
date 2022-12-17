de0167f0edc4ff32fd14ee3271f41f41a16c3335
aliamarliana <106047188+aliamarliana@users.noreply.github.com>
2022-12-17 08:13:21 +0800

    v1.1 UI for user (supervisor)

---
+++ app/Http/Controllers/homeControl.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class homeControl extends Controller
+{
+    public function index(){
+        return view('home');
+    }
+}
